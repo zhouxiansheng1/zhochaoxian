198b2f0228b3e5deb73d3fbed8856b5938e63069
misterzhou <zhochaoxian>
2021-04-11 16:18:52 +0800

    更新1

--- gmall/gmall-bean/src/main/java/com/atguigu/gmall/bean/TestGit.java
+++ gmall/gmall-bean/src/main/java/com/atguigu/gmall/bean/TestGit.java
@@ -2,6 +2,15 @@ package com.atguigu.gmall.bean;
 
 public class TestGit {
     private String git;
+    private String github;
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
 
     public String getGit() {
         return git;
